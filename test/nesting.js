77fb8f611cedad1af0a034f0a59029b6ecbf3117
Jérémy Lal <kapouer@melix.org>
2018-05-24 03:45:12 +0200

    Test nesting

---
+++ test/nesting.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const matchdom = require('../matchdom');
+const dom = require('dom-template-strings');
+require('dom4'); // jsdom is missing .closest
+
+describe('nesting', function() {
+	it('should merge expression in text node expression parameter', function() {
+		let node = dom`<div>[test|or:[def]]</div>`;
+		let copy = matchdom(node, {
+			test: null,
+			def: 'str'
+		});
+		assert.equal(copy.outerHTML, '<div>str</div>');
+	});
+});
+
