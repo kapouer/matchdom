a02a41340f12268d6f4b65037a02d69a10d5d385
Jérémy Lal <kapouer@melix.org>
2018-05-28 09:23:28 +0200

    Test ignore empty expressions

--- test/nesting.js
+++ test/nesting.js
@@ -4,6 +4,12 @@ const dom = require('dom-template-strings');
 require('dom4'); // jsdom is missing .closest
 
 describe('nesting', function() {
+	it('should not crash on empty expressions', function() {
+		let node = dom`<div>[]</div>`;
+		let copy = matchdom(node, {});
+		assert.equal(copy.outerHTML, '<div>[]</div>');
+	});
+
 	it('should merge expression in text node expression parameter', function() {
 		let node = dom`<div>[test|or:[def]]</div>`;
 		let copy = matchdom(node, {
