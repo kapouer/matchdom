5b06c898926dd33da74cb740328c69f4d9a68350
Jérémy Lal <kapouer@melix.org>
2021-01-28 09:40:19 +0100

    flow filters: fix and refactor

--- src/filters/flow.js
+++ src/filters/flow.js
@@ -1,18 +1,14 @@
 export default {
 	then: [null, 'filter', (ctx, val, name, ...params) => {
-		if (val == null) return val;
-		else return ctx.run(name, params);
+		return val ? ctx.run(name, val, ...params) : val;
 	}],
 	and(ctx, val, str) {
-		if (!val) return val;
-		return str;
+		return val ? str : val;
 	},
 	else: [null, 'filter', (ctx, val, name, ...params) => {
-		if (val == null) return val;
-		else return ctx.run(name, params);
+		return val ? val : ctx.run(name, val, ...params);
 	}],
 	or(ctx, val, str) {
-		if (!val) return str;
-		else return val;
+		return val ? val : str;
 	}
 };
