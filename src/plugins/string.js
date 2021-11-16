dd621826d72f12bfc22297350f28548be60514c6
Jérémy Lal <kapouer@melix.org>
2021-11-16 10:54:38 +0100

    enc and dec filters

--- src/plugins/string.js
+++ src/plugins/string.js
@@ -22,5 +22,28 @@ export const filters = {
 	post: ['?', 'str', (x, val, str) => {
 		if (val == null || val === '') return val;
 		return val + str;
+	}],
+	enc: ['?', 'base64|base64url|hex|url', (x, str, type) => {
+		if (!str) return str;
+		str = str.toString();
+		switch (type) {
+			case "base64": return btoa(str);
+			case "base64url": return btoa(str).replace(/=*$/, '');
+			case "url": return encodeURIComponent(str);
+			case "hex": return Array.from(str).map(c => {
+				return c.charCodeAt(0) < 128 ?
+					c.charCodeAt(0).toString(16) :
+					encodeURIComponent(c).replace(/%/g, '').toLowerCase();
+			}).join('');
+		}
+	}],
+	dec: ['string', 'base64|base64url|hex|url', (x, str, type) => {
+		if (!str) return str;
+		switch (type) {
+			case "base64url":
+			case "base64": return atob(str);
+			case "url": return decodeURIComponent(str);
+			case "hex": return decodeURIComponent('%' + str.match(/.{1,2}/g).join('%'));
+		}
 	}]
 };
