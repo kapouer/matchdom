992a116531d36dd8a6b4b6765bd4048936504e2c
Jérémy Lal <kapouer@melix.org>
2023-07-28 12:19:50 +0200

    More test regrouping

--- test/types.js
+++ test/types.js
@@ -86,4 +86,145 @@ describe('types', () => {
 			assert.equal(copy.outerHTML, '<span>test1 - test2</span>');
 		});
 	});
+	describe('parameters', () => {
+		const md = new Matchdom();
+
+		it('should be uri-decoded', () => {
+			const html = `Size[size|const:%3A ]10mm`;
+			const copy = md.merge(html, { size: 10 });
+			assert.equal(copy, 'Size: 10mm');
+		});
+		it('should be left untouched if not uri-decodable', () => {
+			const html = `Percent: [pp][const: %]`;
+			const copy = md.merge(html, { pp: 10 });
+			assert.equal(copy, 'Percent: 10 %');
+		});
+
+		it('should not consider value to be uri-decodable', () => {
+			const html = `?[pp]`;
+			const copy = md.merge(html, { pp: 'test=a%20b' });
+			assert.equal(copy, '?test=a%20b');
+		});
+
+		it('should cast value to int', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['int', (ctx, val) => {
+						assert.equal(val, 10);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:]', { val: "10" });
+			assert.equal(ok, true);
+		});
+		it('should cast param to int', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['int', 'int', (ctx, val, cte) => {
+						assert.equal(cte, 7);
+						ok = true;
+						return val + cte;
+					}]
+				}
+			}).merge('[val|mycheck:7]', { val: 10 });
+			assert.equal(ok, true);
+		});
+		it('should cast param to boolean', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['int', 'bool', (ctx, val, cte) => {
+						assert.equal(cte, true);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:7]', { val: 10 });
+			assert.equal(ok, true);
+		});
+		it('should not check value when type is null', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['any', 'bool', (ctx, val, cte) => {
+						assert.equal(val, 10);
+						assert.equal(cte, false);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:false]', { val: 10 });
+			assert.equal(ok, true);
+		});
+		it('should allow null value when type is null', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['any?', 'bool', (ctx, val, cte) => {
+						assert.equal(val, null);
+						assert.equal(cte, false);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:false]', { val: null });
+			assert.equal(ok, true);
+		});
+
+		it('should allow any value when type is "any"', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['any', 'bool', (ctx, val, cte) => {
+						assert.ok(val instanceof Date);
+						assert.equal(cte, true);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:1]', { val: new Date() });
+			assert.equal(ok, true);
+		});
+		it('should require any value when type is "any"', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['any', 'bool', (ctx, val, cte) => {
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:1]', { val: undefined });
+			assert.equal(ok, false);
+		});
+		it('should get a default empty string value', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['any?', 'bool', (ctx, val, cte) => {
+						assert.equal(val, null);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:1]', { val: null });
+			assert.equal(ok, true);
+		});
+		it('should get a default integer value', () => {
+			let ok = false;
+			md.extend({
+				filters: {
+					mycheck: ['int?10', 'bool', (ctx, val, cte) => {
+						assert.equal(val, 10);
+						ok = true;
+						return val;
+					}]
+				}
+			}).merge('[val|mycheck:1]', { val: null });
+			assert.equal(ok, true);
+		});
+	});
 });
