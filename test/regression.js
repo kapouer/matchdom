f9706e15f7e4a55b400151fecbbbbb9dcb9ae4a4
Jérémy Lal <kapouer@melix.org>
2025-05-20 14:53:40 +0200

    Test recursive expression

--- test/regression.js
+++ test/regression.js
@@ -57,4 +57,58 @@ describe('regressions', () => {
 		const copy = md.merge({ url: `[href|as:url||.query|set:it:4||as:str]` }, obj);
 		assert.equal(copy.url, '/pathname?toto=1&it=4&tata=2');
 	});
+
+
+	it('should not infinitely repeat', () => {
+		const copy = md.merge(`<div>
+			<fieldset>
+				<div>[types|at:fieldset|repeat:mytype|.const]</div>
+				<div>
+					<input type="radio" name="test">
+					<label data-value="[to:value:-input|it.const]">
+					[cases.[mytype.const].list|at:div|repeat:it|.title]
+					</label>
+				</div>
+			</fieldset>
+		</div>`, {
+			types: [{ const: 'a' }, { const: 'b' }],
+			cases: {
+				a: {
+					list: [{ const: '1a', title: 'One' }, { const: '2a', title: 'Two' }]
+				},
+				b: {
+					list: [{ const: '1b', title: 'One' }, { const: '2b', title: 'Two' }, { const: '3b', title: 'Three' }]
+				}
+			}
+		});
+
+		assert.equal(copy.outerHTML.replaceAll(/\t|\n/g, ""), `<div>
+			<fieldset>
+				<div>a</div>
+				<div>
+					<input type="radio" name="test" value="1a">
+					<label>One</label>
+				</div>
+				<div>
+					<input type="radio" name="test" value="2a">
+					<label>Two</label>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div>b</div>
+				<div>
+					<input type="radio" name="test" value="1b">
+					<label>One</label>
+				</div>
+				<div>
+					<input type="radio" name="test" value="2b">
+					<label>Two</label>
+				</div>
+				<div>
+					<input type="radio" name="test" value="3b">
+					<label>Three</label>
+				</div>
+			</fieldset>
+		</div>`.replaceAll(/\t|\n/g, ""));
+	});
 });
